7b34e965550258163a97fc656bd2e3d2dde1844d
Jack Walmsley <jackawalmsley@gmail.com>
2023-07-02 18:04:23 -0400

    Working on mutation

--- js/NEAT.js
+++ js/NEAT.js
@@ -10,7 +10,7 @@ class NEAT {
     }
 
     createPopulation() {
-        while(this.agents.length < this.population) {
+        while (this.agents.length < this.population) {
             let newBrain = new NeuralNetwork(this.numInputs, 0, this.numOutputs);
             let agent = {};
 
@@ -23,28 +23,46 @@ class NEAT {
         }
     }
 
-    performMutations() {
-        for(let agent of this.agents) {
+    performMutations(an, ac, mw) {
+        let totalChance = an + ac + mw;
+        let addNode = an / totalChance;
+        let addConnection = ac / totalChance;
+        let mutateWeight = mw / totalChance;
+
+        for (let agent of this.agents) {
+            if (Math.random() < this.mutationChance) {
+                continue;
+            }
             let genome = this.NetToGenome(agent.brain);
+            let rand = Math.random();
+            if (rand < addNode) {
+                // while()
+                let breakGene = genome[Math.floor(Math.random() * (genome.length - 1))]
+                let input = breakGene.input;
+                let output = breakGene.output;
+                let g1 = new Gene(input, )
+            } else if (rand < addNode + addConnection) {
+            } else if (rand < addNode + addConnection + mutateWeight) {
+            }
         }
     }
 
     NetToGenome(network) {
         let genome = [];
         network.weightsIH.foreach((x, r, c) => {
-            let g = new Gene(c, r, x, true, this.getInnovationNumber(`i${c}`, `h${r}`));
+            let g = new Gene(`i${c}`, `h${r}`, x, true, this.getInnovationNumber(`i${c}`, `h${r}`));
             genome.push(g);
         });
         network.weightsHO.foreach((x, r, c) => {
-            let g = new Gene(c, r, x, true, this.getInnovationNumber(`h${c}`, `o${r}`));
+            let g = new Gene(`h${c}`, `o${r}`, x, true, this.getInnovationNumber(`h${c}`, `o${r}`));
             genome.push(g);
         });
         network.biasHidden.foreach((x, r, c) => {
-            let g = new Gene(c, r, x, true, this.getInnovationNumber(`b`, `h${r}`));
+            let g = new Gene(`b`, `h${r}`, x, true, this.getInnovationNumber(`b`, `h${r}`));
             genome.push(g);
         });
         network.biasOutput.foreach((x, r, c) => {
-            let g = new Gene(c, r, x, true, this.getInnovationNumber(`b`, `o${r}`));
+            let g = new Gene(`b`, `o${r}`, x, true, this.getInnovationNumber(`b`, `o${r}`));
             genome.push(g);
         });
         genome.sort((a, b) => a.innovation - b.innovation);
@@ -53,9 +71,9 @@ class NEAT {
 
     getInnovationNumber(input, output) {
         let ind = -1;
-        for(let i = 0; i < this.genes.length; i++) {
+        for (let i = 0; i < this.genes.length; i++) {
             let gene = this.genes[i]
-            if(gene[0] == input && gene[1]==output) {
+            if (gene[0] == input && gene[1] == output) {
                 ind = i;
             }
         }
@@ -64,6 +82,5 @@ class NEAT {
             ind = this.genes.length - 1;
         }
         return ind;
-        
     }
 }
